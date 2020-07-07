49441ed7704f0d721b3c46a70e0538b93b162e41
Praveen Bisht <prvnbist@gmail.com>
2020-07-07 14:31:06 +0530

    feat: upsert customer

--- src/pages/get-started/register.js
+++ src/pages/get-started/register.js
@@ -2,20 +2,62 @@ import React from 'react'
 import { navigate } from '@reach/router'
 import tw, { styled } from 'twin.macro'
 import { useKeycloak } from '@react-keycloak/web'
+import { useLazyQuery, useMutation } from '@apollo/react-hooks'
 
 import { SEO, Layout, StepsNavbar } from '../../components'
+import { CUSTOMERS, UPDATE_CUSTOMER, CREATE_CUSTOMER } from '../../graphql'
 
 export default () => {
    const [keycloak, initialized] = useKeycloak()
+   const [create] = useMutation(CREATE_CUSTOMER)
+   const [update] = useMutation(UPDATE_CUSTOMER)
+
+   const [customers] = useLazyQuery(CUSTOMERS, {
+      onCompleted: ({ customers }) => {
+         if (customers.length > 0) {
+            const [customer] = customers
+            update({
+               variables: {
+                  id: customer.id,
+                  keycloakId: customer.keycloakId,
+                  _set: { isSubscriber: true },
+               },
+            })
+            if (customer.subscriptionId) {
+               navigate('/')
+            }
+         }
+         if (customers.length === 0) {
+            create({
+               variables: {
+                  object: {
+                     isSubscriber: true,
+                     source: 'subscription',
+                     email: keycloak.userInfo.email,
+                     keycloakId: keycloak.userInfo.sub,
+                     clientId: process.env.GATSBY_CLIENTID,
+                  },
+               },
+            })
+         }
+         navigate('/get-started/select-delivery')
+      },
+   })
 
    React.useEffect(() => {
       if (initialized && keycloak.authenticated) {
          if (window.location !== window.parent.location) {
-            window.parent.location = '/get-started/select-delivery'
+            window.parent.location.reload()
          }
-         navigate('/get-started/select-delivery')
+         if ('tokenParsed' in keycloak) {
+            customers({
+               variables: {
+                  where: { keycloakId: { _eq: keycloak.tokenParsed.sub } },
+               },
+            })
+         }
       }
-   }, [keycloak.authenticated, initialized])
+   }, [keycloak.authenticated, initialized, customers])
 
    return (
       <Layout noHeader>
