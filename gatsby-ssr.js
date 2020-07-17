013f0ee92120a8711b2d9e68718c992a95940802
Praveen Bisht <prvnbist@gmail.com>
2020-07-17 18:37:28 +0530

    fix(build): provider missing

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { ApolloProvider } from './src/lib'
+import { ToastProvider } from 'react-toast-notifications'
 
 export const wrapRootElement = ({ element }) => {
-   return <ApolloProvider>{element}</ApolloProvider>
+   return (
+      <ApolloProvider>
+         <ToastProvider
+            autoDismiss
+            placement="bottom-center"
+            autoDismissTimeout={3000}
+         >
+            {element}
+         </ToastProvider>
+      </ApolloProvider>
+   )
 }
