517882e528d771e731dbee34ce74e7303efccb8d
Praveen Bisht <prvnbist@gmail.com>
2020-07-29 18:54:29 +0530

    feat(section): Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,69 @@
 import React from 'react'
-import { styled } from 'twin.macro'
+import { Link } from 'gatsby'
+import tw, { styled } from 'twin.macro'
 
 import { Header } from './header'
 import { UserProvider } from '../context'
+import { MailIcon, PhoneIcon } from '../assets/icons'
 
 export const Layout = ({ children, noHeader }) => {
    return (
       <UserProvider>
          {!noHeader && <Header />}
          {children}
-         <Footer tw="bg-gray-200">Footer</Footer>
+         <Footer tw="bg-green-600 text-white">
+            <div>
+               <section>
+                  <h2 tw="text-3xl">Subscription Shop</h2>
+                  <p tw="mt-2">
+                     1700 East Willow Street, Signal Hill, California, United
+                     States, 90755
+                  </p>
+                  <span tw="mt-4 flex items-center">
+                     <MailIcon size={18} tw="stroke-current mr-2" />
+                     <a href="mailto:example@example.com" tw="underline">
+                        me@example.com
+                     </a>
+                  </span>
+                  <span tw="mt-4 flex items-center">
+                     <PhoneIcon size={18} tw="stroke-current mr-2" />+ 002-
+                     01008431112
+                  </span>
+               </section>
+               <section>
+                  <h4 tw="text-2xl mb-4 mt-2">Navigation</h4>
+                  <ul>
+                     <li tw="mb-3">
+                        <Link to="/subscription">Home</Link>
+                     </li>
+                     <li tw="mb-3">
+                        <Link to="/subscription/account/profile/">Profile</Link>
+                     </li>
+                     <li tw="mb-3">
+                        <Link to="/subscription/menu">Menu</Link>
+                     </li>
+                  </ul>
+               </section>
+            </div>
+         </Footer>
       </UserProvider>
    )
 }
 
 const Footer = styled.footer`
-   height: 180px;
+   height: 320px;
+   padding: 24px 0;
+   background-size: 160px;
+   background-color: transparent;
+   background-image: url('https://dailykit-assets.s3.us-east-2.amazonaws.com/subs-icons/pattern.png');
+   div {
+      margin: 0 auto;
+      max-width: 980px;
+      width: calc(100% - 40px);
+      ${tw`grid gap-6`}
+      grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+   }
+   @media (max-width: 768px) {
+      height: auto;
+   }
 `
