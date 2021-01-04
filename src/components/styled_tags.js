156c2f066bea64511454c35641d0d5b7a7fa1447
PriyaBihani <priyabihani.official@gmail.com>
2021-01-04 14:35:03 +0530

    feat: hooked up from database

--- src/components/styled_tags.js
+++ src/components/styled_tags.js
@@ -1,5 +1,14 @@
-import tw from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 
-export const Para = tw.p`mb-4`
-export const SubHeading = tw.h2`text-3xl text-gray-700 py-2 my-3 border-b-2 border-gray-200 `
-export const List = tw.ul`pl-6 py-3 list-disc`
+export const StyledArticle = styled.div`
+   h2,
+   h3 {
+      ${tw`text-3xl text-gray-700 py-2 my-3 border-b-2 border-gray-200 `}
+   }
+   p {
+      ${tw`mb-4`}
+   }
+   ul {
+      ${tw`pl-6 py-3 list-disc`}
+   }
+`
