79752304cc47a09090508c5c3eb6abce894c5ef0
Praveen Bisht <prvnbist@gmail.com>
2021-01-05 15:56:22 +0530

    feat: custom login/register

--- src/pages/subscription/login.js
+++ src/pages/subscription/login.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { isEmpty } from 'lodash'
 import { navigate } from 'gatsby'
-import { styled } from 'twin.macro'
+import jwtDecode from 'jwt-decode'
+import tw, { styled } from 'twin.macro'
 import { useKeycloak } from '@react-keycloak/web'
 import { useToasts } from 'react-toast-notifications'
 import { useLazyQuery, useMutation } from '@apollo/react-hooks'
 
-import { useConfig } from '../../lib'
-import { isClient } from '../../utils'
-import { Layout, SEO } from '../../components'
+import { useUser } from '../../context'
+import { useConfig, auth } from '../../lib'
+import { SEO, Layout } from '../../components'
+import { isClient, isKeycloakSupported } from '../../utils'
 import { BRAND, CREATE_CUSTOMER, CUSTOMER } from '../../graphql'
 
 const Login = () => {
+   const { user } = useUser()
    const { brand } = useConfig()
    const [keycloak] = useKeycloak()
    const { addToast } = useToasts()
+   const [current, setCurrent] = React.useState('LOGIN')
 
    const [create_brand_customer] = useMutation(BRAND.CUSTOMER.CREATE, {
       refetchQueries: ['customer'],
@@ -28,6 +32,7 @@ const Login = () => {
    const [create] = useMutation(CREATE_CUSTOMER, {
       refetchQueries: ['customer'],
       onCompleted: () => {
+         dispatch({ type: 'SET_USER', payload: {} })
          navigate('/subscription/get-started/select-delivery')
       },
       onError: () =>
@@ -36,7 +41,7 @@ const Login = () => {
          }),
    })
    const [customer] = useLazyQuery(CUSTOMER.DETAILS, {
-      onCompleted: ({ customer = {} }) => {
+      onCompleted: async ({ customer = {} }) => {
          if (isEmpty(customer)) {
             console.log('CUSTOMER DOESNT EXISTS')
             create({
@@ -44,9 +49,13 @@ const Login = () => {
                   object: {
                      source: 'subscription',
                      sourceBrandId: brand.id,
-                     email: keycloak?.tokenParsed?.email,
+                     email: isKeycloakSupported()
+                        ? keycloak?.tokenParsed?.email
+                        : jwtDecode(localStorage.getItem('token')).email,
                      clientId: process.env.GATSBY_CLIENTID,
-                     keycloakId: keycloak?.tokenParsed?.sub,
+                     keycloakId: isKeycloakSupported()
+                        ? keycloak?.tokenParsed?.sub
+                        : jwtDecode(localStorage.getItem('token')).sub,
                      brandCustomers: {
                         data: {
                            brandId: brand.id,
@@ -58,8 +67,11 @@ const Login = () => {
             return
          }
          console.log('CUSTOMER EXISTS')
+
+         const user = await processUser(customer)
+         dispatch({ type: 'SET_USER', payload: user })
+
          const { brandCustomers = {} } = customer
-         console.log('Login -> brandCustomers', brandCustomers)
          if (isEmpty(brandCustomers)) {
             console.log('BRAND_CUSTOMER DOESNT EXISTS')
             create_brand_customer({
@@ -82,7 +94,15 @@ const Login = () => {
    })
 
    React.useEffect(() => {
-      if (keycloak?.authenticated) {
+      if (user?.keycloakId && !user?.isSubscriber) {
+         navigate('/subscription/get-started/select-delivery')
+      } else if (user?.keycloakId && user?.isSubscriber) {
+         navigate('/subscription/menu')
+      }
+   }, [user])
+
+   React.useEffect(() => {
+      if (isKeycloakSupported() && keycloak?.authenticated) {
          if ('tokenParsed' in keycloak && 'id' in brand) {
             customer({
                variables: {
@@ -95,7 +115,7 @@ const Login = () => {
    }, [keycloak, customer, brand])
 
    React.useEffect(() => {
-      if (isClient) {
+      if (isClient && isKeycloakSupported()) {
          let eventMethod = window.addEventListener
             ? 'addEventListener'
             : 'attachEvent'
@@ -117,20 +137,47 @@ const Login = () => {
    return (
       <Layout>
          <SEO title="Login" />
-         <Main>
-            {keycloak?.authenticated ? (
-               <span>You're already logged in!</span>
+         <Main tw="pt-8">
+            {isKeycloakSupported() ? (
+               <>
+                  {!keycloak?.authenticated && (
+                     <iframe
+                        frameBorder="0"
+                        title="Register"
+                        tw="mx-auto w-full md:w-4/12 h-full"
+                        style={{ height: '780px' }}
+                        src={keycloak?.createRegisterUrl({
+                           redirectUri: isClient
+                              ? `${window.location.origin}/subscription/login-success.xhtml`
+                              : '',
+                        })}
+                     ></iframe>
+                  )}
+               </>
             ) : (
-               <iframe
-                  frameBorder="0"
-                  title="Register"
-                  tw="mx-auto w-full md:w-4/12 h-full"
-                  src={keycloak?.createLoginUrl({
-                     redirectUri: isClient
-                        ? `${window.location.origin}/subscription/login-success.xhtml`
-                        : '',
-                  })}
-               ></iframe>
+               <>
+                  <TabList>
+                     <Tab
+                        className={current === 'LOGIN' ? 'active' : ''}
+                        onClick={() => setCurrent('LOGIN')}
+                     >
+                        Login
+                     </Tab>
+                     <Tab
+                        className={current === 'REGISTER' ? 'active' : ''}
+                        onClick={() => setCurrent('REGISTER')}
+                     >
+                        Register
+                     </Tab>
+                  </TabList>
+                  {current === 'LOGIN' && <LoginPanel customer={customer} />}
+                  {current === 'REGISTER' && (
+                     <RegisterPanel
+                        setCurrent={setCurrent}
+                        customer={customer}
+                     />
+                  )}
+               </>
             )}
          </Main>
       </Layout>
@@ -139,10 +186,239 @@ const Login = () => {
 
 export default Login
 
+const LoginPanel = ({ customer }) => {
+   const { brand } = useConfig()
+   const [error, setError] = React.useState('')
+   const [form, setForm] = React.useState({
+      email: '',
+      password: '',
+   })
+
+   const isValid = form.email && form.password
+
+   const onChange = e => {
+      const { name, value } = e.target
+      setForm(form => ({
+         ...form,
+         [name]: value,
+      }))
+   }
+
+   const submit = async () => {
+      try {
+         setError('')
+         const token = await auth.login({
+            email: form.email,
+            password: form.password,
+         })
+         if (token?.sub) {
+            customer({
+               variables: {
+                  keycloakId: token?.sub,
+                  brandId: brand.id,
+               },
+            })
+         } else {
+            setError('Failed to login, please try again!')
+         }
+      } catch (error) {
+         if (error?.code === 401) {
+            setError('Email or password is incorrect!')
+         }
+      }
+   }
+
+   return (
+      <Panel>
+         <FieldSet>
+            <Label htmlFor="email">Email</Label>
+            <Input
+               type="email"
+               name="email"
+               value={form.email}
+               onChange={onChange}
+               placeholder="Enter your email"
+            />
+         </FieldSet>
+         <FieldSet>
+            <Label htmlFor="password">Password</Label>
+            <Input
+               name="password"
+               type="password"
+               onChange={onChange}
+               value={form.password}
+               placeholder="Enter your password"
+            />
+         </FieldSet>
+         <Submit
+            className={!isValid ? 'disabled' : ''}
+            onClick={() => isValid && submit()}
+         >
+            Login
+         </Submit>
+         {error && <span tw="self-start block text-red-500 mt-2">{error}</span>}
+      </Panel>
+   )
+}
+
+const RegisterPanel = ({ customer, setCurrent }) => {
+   const { brand } = useConfig()
+   const [error, setError] = React.useState('')
+   const [form, setForm] = React.useState({
+      email: '',
+      password: '',
+   })
+
+   const isValid = form.email && form.password
+
+   const onChange = e => {
+      const { name, value } = e.target
+      setForm(form => ({
+         ...form,
+         [name]: value,
+      }))
+   }
+
+   const submit = async () => {
+      try {
+         setError('')
+         const result = await auth.register({
+            email: form.email,
+            password: form.password,
+         })
+         if (result?.success) {
+            const token = await auth.login({
+               email: form.email,
+               password: form.password,
+            })
+            if (token?.sub) {
+               customer({
+                  variables: {
+                     keycloakId: token?.sub,
+                     brandId: brand.id,
+                  },
+               })
+            }
+         }
+      } catch (error) {
+         if (error.includes('exists')) {
+            return setError('Email is already in use!')
+         }
+         setError('Failed to register, please try again!')
+      }
+   }
+
+   return (
+      <Panel>
+         <FieldSet>
+            <Label htmlFor="email">Email</Label>
+            <Input
+               type="email"
+               name="email"
+               value={form.email}
+               onChange={onChange}
+               placeholder="Enter your email"
+            />
+         </FieldSet>
+         <FieldSet>
+            <Label htmlFor="password">Password</Label>
+            <Input
+               name="password"
+               type="password"
+               onChange={onChange}
+               value={form.password}
+               placeholder="Enter your password"
+            />
+         </FieldSet>
+         <Submit
+            className={!isValid ? 'disabled' : ''}
+            onClick={() => isValid && submit()}
+         >
+            Register
+         </Submit>
+         <button
+            tw="self-start mt-2 text-blue-500"
+            onClick={() => setCurrent('LOGIN')}
+         >
+            Login instead?
+         </button>
+         {error && <span tw="self-start block text-red-500 mt-2">{error}</span>}
+      </Panel>
+   )
+}
+
+const processUser = customer => {
+   const sub = {}
+   const { brandCustomers = [], ...rest } = customer
+
+   if (!isEmpty(brandCustomers)) {
+      const [brand_customer] = brandCustomers
+
+      const {
+         subscription = null,
+         subscriptionId = null,
+         subscriptionAddressId = null,
+         subscriptionPaymentMethodId = null,
+      } = brand_customer
+
+      rest.subscription = subscription
+      rest.subscriptionId = subscriptionId
+      rest.subscriptionAddressId = subscriptionAddressId
+      rest.subscriptionPaymentMethodId = subscriptionPaymentMethodId
+
+      sub.defaultAddress = rest?.platform_customer?.addresses.find(
+         address => address.id === subscriptionAddressId
+      )
+
+      sub.defaultPaymentMethod = rest?.platform_customer?.paymentMethods.find(
+         method => method.stripePaymentMethodId === subscriptionPaymentMethodId
+      )
+   }
+   return { ...rest, ...sub }
+}
+
 const Main = styled.main`
    margin: auto;
    overflow-y: auto;
    max-width: 1180px;
    width: calc(100vw - 40px);
-   height: calc(100vh - 128px);
+   min-height: calc(100vh - 128px);
+   > section {
+      width: 100%;
+      max-width: 360px;
+   }
+`
+
+const Panel = styled.section`
+   ${tw`flex mx-auto justify-center items-center flex-col py-4`}
+`
+
+const TabList = styled.ul`
+   ${tw`border-b flex justify-center space-x-3`}
+`
+
+const Tab = styled.button`
+   ${tw`h-8 px-3`}
+   &.active {
+      ${tw`border-b border-green-500 border-b-2`}
+   }
+`
+
+const FieldSet = styled.fieldset`
+   ${tw`w-full flex flex-col mb-4`}
+`
+
+const Label = styled.label`
+   ${tw`text-gray-600 mb-1`}
+`
+
+const Input = styled.input`
+   ${tw`w-full block border h-10 rounded px-2 outline-none focus:border-2 focus:border-blue-400`}
+`
+
+const Submit = styled.button`
+   ${tw`bg-green-500 rounded w-full h-10 text-white uppercase tracking-wider`}
+   &.disabled {
+      ${tw`cursor-not-allowed bg-gray-300 text-gray-700`}
+   }
 `
