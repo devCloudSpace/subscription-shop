fcc16c47ccd71ffcde061537ddc69565f834f815
Praveen Bisht <prvnbist@gmail.com>
2021-02-24 11:42:04 +0530

    feat: tag based week selector

--- src/sections/select-menu/week_picker.js
+++ src/sections/select-menu/week_picker.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import moment from 'moment'
-import tw, { styled, css } from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 
 import { useMenu } from './state'
 import { Loader } from '../../components'
-import { formatDate } from '../../utils'
-import { ArrowLeftIcon, ArrowRightIcon } from '../../assets/icons'
 
 export const WeekPicker = ({ isFixed }) => {
    const { state, dispatch } = useMenu()
@@ -33,74 +31,54 @@ export const WeekPicker = ({ isFixed }) => {
 
    if (state.isOccurencesLoading) return <Loader inline />
    if (!state?.week?.id) return null
-   return (
-      <Occurence isFixed={isFixed}>
-         {!isFixed && (
-            <SliderButton onClick={() => previous()}>
-               <ArrowLeftIcon css={tw`stroke-current text-green-800`} />
-            </SliderButton>
-         )}
-         <span
-            css={tw`flex items-center justify-center text-base text-center md:text-lg text-indigo-800`}
-         >
-            Showing menu of:&nbsp;
-            {formatDate(
-               moment(state?.week?.fulfillmentDate)
+   if (isFixed)
+      return (
+         <Occurence isFixed={isFixed}>
+            <span
+               css={tw`flex items-center justify-center text-base text-center md:text-lg text-indigo-800`}
+            >
+               Showing menu of:&nbsp;
+               {moment(state?.week?.fulfillmentDate)
                   .subtract(7, 'days')
-                  .format('YYYY-MM-DD'),
-               {
-                  month: 'short',
-                  day: 'numeric',
-                  year: 'numeric',
+                  .format('MMM D')}
+               &nbsp;-&nbsp;
+               {moment(state?.week?.fulfillmentDate).format('MMM D')}
+            </span>
+         </Occurence>
+      )
+   return (
+      <Occurences>
+         {state.occurences.map(occurence => (
+            <Occurence
+               onClick={() =>
+                  dispatch({ type: 'SET_WEEK', payload: occurence })
                }
-            )}
-            &nbsp;-&nbsp;
-            {formatDate(state?.week?.fulfillmentDate, {
-               month: 'short',
-               day: 'numeric',
-               year: 'numeric',
-            })}
-         </span>
-         {!isFixed && (
-            <SliderButton onClick={() => next()}>
-               <ArrowRightIcon css={tw`stroke-current text-green-800`} />
-            </SliderButton>
-         )}
-      </Occurence>
+               className={
+                  state.week?.fulfillmentDate === occurence.fulfillmentDate
+                     ? 'active'
+                     : ''
+               }
+            >
+               {moment(occurence?.fulfillmentDate)
+                  .subtract(7, 'days')
+                  .format('MMM D')}
+               &nbsp;-&nbsp;
+               {moment(occurence?.fulfillmentDate).format('MMM D')}
+            </Occurence>
+         ))}
+      </Occurences>
    )
 }
 
-const Occurence = styled.div(
-   ({ isFixed }) => css`
-      height: 64px;
-      display: grid;
-      margin: auto;
-      max-width: 980px;
-      width: 100%;
-      grid-template-columns: ${isFixed ? '1fr' : '64px 1fr 64px'};
-      @media (max-width: 567px) {
-         grid-template-columns: ${isFixed ? '1fr' : '48px 1fr 48px'};
-         position: fixed;
-         background-color: #fff;
-         z-index: 20;
-         top: 64px;
-      }
-   `
-)
+const Occurences = styled.ul`
+   height: 64px;
+   max-width: 980px;
+   ${tw`w-full mx-auto overflow-x-auto flex items-center justify-center space-x-4`}
+`
 
-const SliderButton = styled.button`
-   width: 48px;
-   height: 48px;
-   ${tw`
-      mx-2
-      self-center
-      rounded-full
-      hover:bg-gray-100
-      border border-green-800 
-      flex items-center justify-center 
-   `}
-   @media (max-width: 567px) {
-      width: 32px;
-      height: 32px;
+const Occurence = styled.li`
+   ${tw`flex-shrink-0 px-3 rounded-full border`}
+   &.active {
+      ${tw`bg-green-200 text-green-700`}
    }
 `
